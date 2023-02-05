23596558d2c0174ddedb4b2e13e9976128fb36f8
Jackie Dong <70934958+jackiedong6@users.noreply.github.com>
2023-02-05 16:11:03 -0500

    new backend api

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -6,7 +6,7 @@ import axios from "axios";
 
 // const backend_url = "http://localhost:5000/api"
 
-const backend_url = "https://api.yalemajors.com/api/auth/cas"
+const backend_url = "https://api.yalemajors.com/api"
 
 export default axios.create({
     withCredentials: true,
